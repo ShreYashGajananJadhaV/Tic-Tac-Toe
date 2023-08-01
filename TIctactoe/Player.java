93a10ef86c6714beee4577c137564989e38597d3
Shreyash Gajanan Jadhav <113574743+ShreYashGajananJadhaV@users.noreply.github.com>
2023-08-01 16:27:10 +0530

    Update Player.java

--- TIctactoe/Player.java
+++ TIctactoe/Player.java
@@ -7,7 +7,7 @@ public class Player {
     boolean chance;
     String name;
     HashMap<Integer, int[]> hashmap = new HashMap<Integer, int[]>();
-    public Player(int a,String name){
+    public Player(String name){
         this.hashmap.put(2, new int[]{1, 3, 5, 8});
         this.hashmap.put(4, new int[]{1, 7, 5, 6});
         this.hashmap.put(8, new int[]{7,9,5,2});
@@ -23,22 +23,5 @@ public class Player {
         this.name = name;
         this.status = false;
         this.chance  = true;
-    }
-    public Player(String name){
-        this.hashmap.put(2, new int[]{1, 3, 5, 8});
-        this.hashmap.put(4, new int[]{1, 7, 5, 6});
-        this.hashmap.put(8, new int[]{7,9,5,2});
-        this.hashmap.put(6, new int[]{3,9,4,5});
-
-        this.hashmap.put(5, new int[]{2,8,4,6,1,9,7,3});
-
-        this.hashmap.put(1, new int[]{2,3,5,9,4,7});
-        this.hashmap.put(3, new int[]{1,2,6,9,7,5});
-        this.hashmap.put(9, new int[]{3,6,7,8,1,5});
-        this.hashmap.put(7, new int[]{1,4,5,3,8,9});
-        arr = new ArrayList<Integer>();
-        this.name = name;
-        this.status = false;
-        this.chance = false;
     }
 }
